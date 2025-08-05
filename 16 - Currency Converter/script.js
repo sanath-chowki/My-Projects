4e0ded304f802d6b46e5445192da0fa3ad6cfa48
Sanath Chowki <sanathchowki24@gmail.com>
2025-08-05 13:30:13 +0530

    Currency Converter

---
+++ 16 - Currency Converter/script.js
@@ -0,0 +1,24 @@
+console.log("script.js is working...")
+const btn = document.querySelector(".btn")
+const populate = async (value, currency) => {
+    let myStr=""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_Gni3dXjaDKMLfJS9n5BCsYbt5vPHcrtRcUbeSnwl&base_currency="+currency
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
+    for(let key of Object.keys(rJson["data"])) {
+        myStr +=   `<tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"]*value)}</td>
+                    </tr>`
+    }
+    const tableBody = document.querySelector("tbody")
+    tableBody.innerHTML = myStr
+}
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value)
+    const currency = document.querySelector("select[name='currency']").value
+    populate(value, currency)
+})
