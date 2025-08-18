2eaccaaa17aa57280ca49a3699db04c962860661
Sanath Chowki <sanathchowki24@gmail.com>
2025-08-18 15:12:53 +0530

    Random Emoji

---
+++ 36 - Random Emoji/script.js
@@ -0,0 +1,19 @@
+const btnEl=document.getElementById('btn')
+const emojiNameEl=document.getElementById('emoji-name')
+const emoji=[]
+async function getEmoji() {
+    let response=await fetch("https://emoji-api.com/emojis?access_key=96703cc9b52a8d6cc35dfaf342733a3ab88b140b")
+    data=await response.json()
+    for(let i=0; i<1500; i++) {
+        emoji.push({
+            emojiName: data[i].character,
+            emojiCode: data[i].unicodeName,
+        })
+    }
+}
+getEmoji()
+btnEl.addEventListener('click', ()=>{
+    const randomNum=Math.floor(Math.random()*emoji.length)
+    btnEl.innerText=emoji[randomNum].emojiName
+    emojiNameEl.innerText=emoji[randomNum].emojiCode
+})
