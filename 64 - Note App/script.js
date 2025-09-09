57475a0cf26a26f0b415b2fcc710c005e39d35d8
Sanath Chowki <sanathchowki24@gmail.com>
2025-09-09 11:50:33 +0530

    Note App

---
+++ 64 - Note App/script.js
@@ -0,0 +1,51 @@
+const btnEl=document.getElementById("btn")
+const appEl=document.getElementById("app")
+btnEl.addEventListener("click", addNote)
+function addNote() {
+    const noteObj={
+        id: Math.floor(Math.random()*100000),
+        content: ""
+    }
+    const noteEl=createNoteEl(noteObj.id, noteObj.content)
+    appEl.insertBefore(noteEl, btnEl)
+    const notes=getNotes()
+    notes.push(noteObj)
+    saveNote(notes)
+}
+function createNoteEl(id, content) {
+    const element=document.createElement("textarea")
+    element.classList.add("note")
+    element.placeholder="Empty Note"
+    element.value=content
+    element.addEventListener("dblclick", ()=>{
+        const warning=confirm("Do you want to delete this note ?")
+        if(warning) {
+            deleteNote(id, element)
+        }
+    })
+    element.addEventListener("input", ()=>{
+        updateNote(id, element.value)
+    })
+    return element
+}
+function saveNote(notes) {
+    localStorage.setItem("note-app", JSON.stringify(notes))
+}
+function getNotes() {
+    return JSON.parse(localStorage.getItem("note-app")||"[]")
+}
+getNotes().forEach((note)=>{
+    const noteEl=createNoteEl(note.id, note.content)
+    appEl.insertBefore(noteEl, btnEl)
+})
+function updateNote(id, content) {
+    const notes=getNotes()
+    const target=notes.filter((note)=>note.id==id)[0]
+    target.content=content
+    saveNote(notes)
+}
+function deleteNote(id, element) {
+    const notes=getNotes().filter((note)=>note.id!=id)
+    saveNote(notes)
+    appEl.removeChild(element)
+}
